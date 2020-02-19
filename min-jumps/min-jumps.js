8b7f91db5f3cb4acc13cf87ff198fe1f0b8769b2
Alexander Wang <alexander.wang22@gmail.com>
2020-02-19 16:45:49 -0500

    complete

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const minJumps = arr => {
+  let jump = 1;
+  if (arr.length < 1) return 0;
+  if (arr[0] >= arr.length - 1) return 1;
+  let nextJump = arr.slice(1, arr[0] + 1);
+  let max = Math.max(...nextJump);
+  // console.log("TCL: max", max);
+  let nextJumpIndex = arr.indexOf(max);
+  // console.log("TCL: nextJumpIndex", nextJumpIndex);
 
+  jump += minJumps(arr.slice(nextJumpIndex));
+  return jump;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
